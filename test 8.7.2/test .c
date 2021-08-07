dffa14994d3d5ec26b6d1cc7e405c61876e15bdf
qkja <2660015902@qq.com>
2021-08-07 21:42:17 +0800

    提交代码

---
+++ test 8.7.2/test .c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include"test.h"
+//static int v_gal = 10;
+//void print(void)
+//{
+//	printf("hehehe\n");
+//}
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//static void print2(int n)
+//{
+//	printf("hello bit :%d\n", n);
+//	printf("hello v_gal :%d\n", v_gal);
+//}
+//void print2_helper()
+//{
+//	print2(20);
+//}
+//int test(int x)
+//{
+//    return x < 2 ? x : test(x - 1) * x;
+//}
+//void test()
+//{
+//  #include<stdio.h>
+//  #include<windows.h>
+//}
---
+++ test 8.7.2/test.h
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#pragma once
+//// C头文件
+////所有的变量的声明
+////所有的函数的声明
+////#define 类型 
+#include<stdio.h>
+#include<windows.h>
+//extern int v_gal;            //可不带，但我一定要带，可读性
+////函数的声明
+//extern void print();         //可不带，建议带
+//extern int Add(int x, int y);//一定要带参数
+//extern void print2(int n);
+//extern void print2_helper();
+//extern int test(int x);
+extern void test();
