bbe538b098f3965335a119fea49f3eebcc8b6b96
qkja <2660015902@qq.com>
2021-08-11 16:22:02 +0800

    提交代码

--- test .8.8/test .h
+++
@@ -1,4 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#pragma once
-#include<stdio.h>
-#include<string.h>
--- test7.27/add.c
+++
@@ -1,5 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-int Add(int a, int b)
-{
-	return a + b;
-}
